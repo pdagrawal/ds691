8577ac1dd6a22e1e26a3c54eead5a7da7a97b9bb
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-04-26 14:04:58 -0400

    optimized the file import code

--- assignment_3/assignment_3_q5.py
+++ assignment_3/assignment_3_q5.py
@@ -1,38 +1,19 @@
 import pymongo
 import csv
+import os
 
 client = pymongo.MongoClient()
 db = client['mymongodb']
+data_dir = 'data'
 
-movies_collection = db['movies']
-ratings_collection = db['ratings']
-tags_collection = db['tags']
-
-movies_list = []
-with open("data/movies.csv", 'r') as f:
-    csvreader = csv.reader(f)
-    header = next(csvreader)
-    for row in csvreader:
-    	movies_list.append({'_id': row[0], 'title': row[1], 'genres': row[2]})
-
-results = movies_collection.insert_many(movies_list)
-
-
-ratings_list = []
-with open("data/ratings.csv", 'r') as f:
-    csvreader = csv.reader(f)
-    header = next(csvreader)
-    for row in csvreader:
-    	ratings_list.append({'userId': row[0], 'movieId': row[1], 'rating': row[2], 'timestamp': row[3]})
-
-ratings = ratings_collection.insert_many(ratings_list)
-
-
-tags_list = []
-with open("data/tags.csv", 'r') as f:
-    csvreader = csv.reader(f)
-    header = next(csvreader)
-    for row in csvreader:
-    	tags_list.append({'userId': row[0], 'movieId': row[1], 'tag': row[2], 'timestamp': row[3]})
-
-tags = tags_collection.insert_many(tags_list)
+for filename in os.listdir(data_dir):
+    data_list = []
+    with open(data_dir + '/' + filename, 'r') as f:
+        csvreader = csv.reader(f)
+        header = next(csvreader)
+        for row in csvreader:
+            data = {}
+            for index, column in enumerate(header):
+                data[column] = row[index]
+            data_list.append(data)
+    db[filename.split('.')[0]].insert_many(data_list)
